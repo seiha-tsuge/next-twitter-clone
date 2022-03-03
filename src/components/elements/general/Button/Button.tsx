d1d76c427aea0847c776d57ac0711425084764d1
seiha-tsgue <seiha.tsuge@gmail.com>
2022-03-03 11:53:01 +0900

    Buttonコンポーネントの作成

---
+++ src/components/elements/general/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Button as MantineButton } from '@mantine/core';
+import type { ButtonProps as MantineButtonProps } from '@mantine/core';
+
+export const Button: React.VFC<MantineButtonProps<any>> = ({ children, sx, ...props }) => {
+  return (
+    <MantineButton sx={() => ({ ...sx })} {...props}>
+      {children}
+    </MantineButton>
+  );
+};

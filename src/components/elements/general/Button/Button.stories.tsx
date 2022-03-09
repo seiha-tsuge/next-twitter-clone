12fb4f343d0779353e99ec838620268eb19c4aa5
seiha-tsgue <seiha.tsuge@gmail.com>
2022-03-09 23:37:32 +0900

    ストリーブックを追加

---
+++ src/components/elements/general/Button/Button.stories.tsx
@@ -0,0 +1,42 @@
+import { ComponentStoryObj, ComponentMeta } from '@storybook/react';
+
+import { Button } from './Button';
+
+export default {
+  title: 'Button',
+  component: Button,
+  parameters: {
+    controls: { expanded: true },
+  },
+} as ComponentMeta<typeof Button>;
+
+type Story = ComponentStoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: {
+    children: 'ボタン',
+  },
+  argTypes: {
+    compact: {
+      control: 'boolean',
+    },
+    fullWidth: {
+      control: 'boolean',
+    },
+  },
+  parameters: {
+    controls: { expanded: true },
+  },
+};
+
+export const Variant: Story = {
+  render: (args) => (
+    <div style={{ display: 'flex', gap: '16px' }}>
+      <Button variant='subtle'>ボタン</Button>
+      <Button variant='light'>ボタン</Button>
+      <Button variant='filled'>ボタン</Button>
+      <Button variant='outline'>ボタン</Button>
+      <Button variant='default'>ボタン</Button>
+    </div>
+  ),
+};
